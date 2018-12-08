94a23c9ccdda087a3cd1494e938771cd01dd60a8
EdgeWalker <aegookja@naver.com>
2018-12-08 15:42:04 +0100

    Added ShellExplosion
    Added ShellExplosion
    Moved some scripts

--- Assets/Scripts/ShellController.cs
+++ Assets/Scripts/ShellController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ShellController : MonoBehaviour
 {
 	public float speed;
+	public ParticleSystem explosion;
 
 	private void Update()
 	{
 		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+
+	private void OnDisable()
+	{
+		explosion.transform.position = transform.position;
+		explosion.Play();
 	}
 }
