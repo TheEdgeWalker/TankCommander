2a1240b6c1f345cc5bccfd2a9ce12b4201abf4a0
EdgeWalker <aegookja@naver.com>
2018-12-08 22:05:56 +0100

    implemented Tank status bar
    Tank status bar

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,10 +7,11 @@ public class TankController : MonoBehaviour
 {
 	public Transform turret;
 
+	public int hitPoint = 100;
+	public int actionPoint = 100;
+
 	private NavMeshAgent agent;
 	private TankHitbox[] hitboxes;
-
-	private int health = 100;
 
 	private void Awake()
 	{
@@ -25,17 +26,22 @@ public class TankController : MonoBehaviour
 
 	private void Update()
 	{
-		if (health <= 0)
+		if (hitPoint <= 0)
 		{
 			gameObject.SetActive(false);
 		}
 	}
 
+	public bool IsBusy()
+	{
+		return agent.pathPending || agent.velocity.magnitude > 0f;
+	}
+
 	public void SetDestination(Vector3 destination)
 	{
-		if (agent.velocity.magnitude > 0f)
+		if (IsBusy())
 		{
-			Debug.Log("Agent is moving, cannot set new destination");
+			Debug.Log("Agent is busy, cannot set new destination");
 			return;
 		}
 
@@ -51,11 +57,17 @@ public class TankController : MonoBehaviour
 
 	public void Fire()
 	{
+		if (IsBusy())
+		{
+			Debug.Log("Agent is busy, cannot fire");
+			return;
+		}
+
 		ShellManager.instance.Fire(turret);
 	}
 
 	public void RecieveDamage(int damage)
 	{
-		health -= damage;
+		hitPoint -= damage;
 	}
 }
---
+++ Assets/Scripts/TankStatusBars.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankStatusBars : MonoBehaviour
+{
+	public TankController tankController;
+	public Slider hitPointSlider;
+	public Slider actionPointSlider;
+
+	private void Update()
+	{
+		if (tankController == null)
+		{
+			return;
+		}
+
+		if (!tankController.gameObject.activeInHierarchy)
+		{
+			gameObject.SetActive(false);
+		}
+
+		hitPointSlider.value = tankController.hitPoint;
+		actionPointSlider.value = tankController.actionPoint;
+
+		Vector3 screenPoint = Camera.main.WorldToScreenPoint(tankController.transform.position);
+		transform.position = screenPoint + (Vector3.up * 40f);
+	}
+}
